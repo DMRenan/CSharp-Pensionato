c6801c48816a471e4373a90a1c06710c6d2c6e27
Renan Duarte <rnnmrcdrt@gmail.com>
2022-04-18 12:43:05 -0300

    First commit

---
+++ Pensionato/Estudante.cs
@@ -0,0 +1,19 @@
+﻿namespace Pensionato
+{
+    internal class Estudante
+    {
+        string Nome { get; set; }
+        string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ Pensionato/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Pensionato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vetorQuartos = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int qtdquartos = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= qtdquartos; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vetorQuartos[quarto-1] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vetorQuartos[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vetorQuartos[i]);
+                }
+            }
+        }
+    }
+}
